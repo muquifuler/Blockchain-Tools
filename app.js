d41f4319f18a865f4d69a909825803e7dc85abaa
mukas <57547835+muquifuler@users.noreply.github.com>
2022-02-22 22:46:23 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+function token(){
+    window.location.href = "./btools/token.html";
+}
+function home(){
+    window.location.href = "../index.html";
+}
+function retoken(){
+    window.location.href = "./token.html";
+}
+
+//Tokens
+
+function erc20(){
+    window.location.href = "./token-erc20.html";
+}
